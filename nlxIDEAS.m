55706e73c8ef04204858888873fd4c5c5a2238f1
John Thompson <john.a.thompson@cuanschutz.edu>
2021-03-26 13:32:27 -0600

    update

---
+++ nlxIDEAS.m
@@ -0,0 +1,4 @@
+The Header includes useful information such as type of file, version of Cheetah and type of system used, sampling frequency, input range, filter settings, and ADBitVolts value. The ADBitVolts value is important when importing data into another analysis tool such as MATLAB®. The value indicated is the multiplier used in conjunction with the A/D value stored in each record from each sample. For example:
+
+
+(Index 4: Sample4 value= 579) x (ADBitVolt value= 0.000000030518510385491027) = 0.000017670217513199304633 Volts or 17.67 µV
