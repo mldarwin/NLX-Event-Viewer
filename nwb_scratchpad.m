2919329d0ac56221af87dc9acb354f792d911ee4
johnarthurthompson <john.a.thompson@cuanschutz.edu>
2021-04-26 19:55:06 -0600

    update

--- nwb_scratchpad.m
+++ nwb_scratchpad.m
@@ -1,13 +1,23 @@
-
+nwb2 = nwbRead('MLD1SessN1.nwb');
+t = nwb2.acquisition.get('ElectricalSeries_MA');
+t = nwb2.acquisition.get('ElectricalSeries_MA').data.load();
+nwb2.general_extracellular_ephys_electrodes.vectordata.get('shortN').data.load(2);
 
 
 
 electrodes_object_view = types.untyped.ObjectView( ...
     '/general/extracellular_ephys/electrodes');
+
+
+
+
 electrode_table_region1 = types.hdmf_common.DynamicTableRegion( ...
     'table', electrodes_object_view, ...
     'description', 'some electrodes', ...
     'data', (0:10)');
+
+
+
 electrode_table_region2 = types.hdmf_common.DynamicTableRegion( ...
     'table', electrodes_object_view, ...
     'description', 'some different electrodes', ...
