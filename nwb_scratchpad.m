9b268ae33084efde1e500e68043b37566465c6a7
johnarthurthompson <john.a.thompson@cuanschutz.edu>
2021-05-17 06:42:22 -0600

    update

--- nwb_scratchpad.m
+++ nwb_scratchpad.m
@@ -1,46 +1,14 @@
-nwb2 = nwbRead('MLD1SessN1.nwb');
-t = nwb2.acquisition.get('ElectricalSeries_MA');
-t = nwb2.acquisition.get('ElectricalSeries_MA').data.load();
-nwb2.general_extracellular_ephys_electrodes.vectordata.get('shortN').data.load(2);
+nwb2 = nwbRead('MLD1_Session6_raw.nwb');
+
+macroDat = nwb2.acquisition.get('MacroWireSeries').data.load();
+adbitVal = nwb2.acquisition.get('MacroWireSeries').data_conversion;
+macroDATl = macroDat(:,1); % First channel, all samples
+macroDATla = macroDATl*adbitVal;
+
+FS = nwb2.acquisition.get('MacroWireSeries').starting_time_rate;
+
+nwb2.general_extracellular_ephys_electrodes.vectordata.get('shortBAn').data.load(2)
 
 
 
-electrodes_object_view = types.untyped.ObjectView( ...
-    '/general/extracellular_ephys/electrodes');
 
-
-
-
-electrode_table_region1 = types.hdmf_common.DynamicTableRegion( ...
-    'table', electrodes_object_view, ...
-    'description', 'some electrodes', ...
-    'data', (0:10)');
-
-
-
-electrode_table_region2 = types.hdmf_common.DynamicTableRegion( ...
-    'table', electrodes_object_view, ...
-    'description', 'some different electrodes', ...
-    'data', (11:21)');
-
-nwb.acquisition.set('MicrowireSeries', electrical_series_1);
-nwb.acquisition.set('MacrowireSeries', electrical_series_2);
-
-data = nw.acquisition.get('MicrowireSeries').electrodes();
-table(transpose(data));
-tbl1 = array2table(transpose(data));
-stackedplot(tbl1)
-
-
-nw2 = nwbRead('nwb');
-micInd = nw2.acquisition.get('MicroWireSeries').electrodes.data.load();
-lt = nw2.general_extracellular_ephys_electrodes.vectordata.get('label').data.load();
-ltc = cellstr(lt);
-ltc(micInd+1);
-
-
-%% 
-
-% create LFP processing
-% add behavior events
-% create spike processing
